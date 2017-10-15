4b4bebc6b1c8dd41a9373976da0546af68cb3bb0
Bobur <github.bobur@gmail.com>
2017-10-15 22:40:26 +1100

    changing Dice.java

--- src/game/Dice.java
+++ src/game/Dice.java
@@ -1,21 +1,23 @@
 package game;
-public class Dice {
-			
-	private DiceValue value;
-	
-	public Dice() {
-		value =  DiceValue.getRandom();
-	}
-	
-	public DiceValue getValue() {
-		return value;
-	}
 
-	public DiceValue roll() {
-		return DiceValue.getRandom();
-	}		
-	
-	public String toString() {
-		return value.toString();
-	}
+public class Dice {
+
+    private DiceValue value;
+
+    public Dice() {
+        value = DiceValue.getRandom();
+    }
+
+    public DiceValue getValue() {
+        return value;
+    }
+
+    public DiceValue roll() {
+        value = DiceValue.getRandom();
+        return value;
+    }
+
+    public String toString() {
+        return value.toString();
+    }
 }
